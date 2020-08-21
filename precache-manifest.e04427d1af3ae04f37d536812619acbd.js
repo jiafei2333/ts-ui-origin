fb060d3e8f26f675e0d0f115bd5ff393d917893a
jiafei2333 <791916405@qq.com>
2020-08-21 03:23:04 +0000

    Deploying to gh-pages from  @ d9620c9bb40cb4de0443c46350fda9e3e7f4beb4 🚀

---
+++ precache-manifest.e04427d1af3ae04f37d536812619acbd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c926f1fab31449cdc06417d327c07ec6",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.85a3edd73c2b1ce6ad3d.bundle.js"
+  },
+  {
+    "url": "runtime~main.85a3edd73c2b1ce6ad3d.bundle.js"
+  },
+  {
+    "url": "vendors~main.85a3edd73c2b1ce6ad3d.bundle.js"
+  },
+  {
+    "url": "vendors~main.85a3edd73c2b1ce6ad3d.bundle.js.LICENSE.txt"
+  }
+]);
